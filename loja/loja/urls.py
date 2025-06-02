38dfe132c2f367080c6f4024d5e385775047289d
Micaela Tamires <micaela.tamir@gmail.com>
2025-06-02 15:08:15 -0300

    update django

--- loja/loja/urls.py
+++ loja/loja/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # adiciona o include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('vitrine.urls')),  # conecta com as URLs do app vitrine
+ ]
